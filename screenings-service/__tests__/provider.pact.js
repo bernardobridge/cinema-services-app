714727d586fac6d8e38e62237dbff93a3bb274dd
Bernardo Guerreiro <bernardoguerr@gmail.com>
2020-02-02 22:12:29 +0000

    tests: adds screenings service provider verification step

---
+++ screenings-service/__tests__/provider.pact.js
@@ -0,0 +1,31 @@
+const { Verifier } = require('@pact-foundation/pact');
+
+describe('PACT Verification', () => {
+    let app;
+
+    beforeAll(() => {
+        process.env.SERVICE_PORT = 4002;
+        app = require('../app');
+        app.listen(process.env.SERVICE_PORT, () => {
+            console.log(`App running on port ${process.env.SERVICE_PORT}.`)
+        })
+    });
+
+    it('should verify expected interactions', () => {
+        const opts = {
+            pactBrokerUrl: process.env.PACT_BROKER_URL || 'https://limitless-hamlet-75386.herokuapp.com/',
+            pactBrokerUsername: process.env.PACT_BROKER_USERNAME || 'admin',
+            pactBrokerPassword: process.env.PACT_BROKER_PASSWORD || 'pacty123',
+            provider: 'Screenings Service',
+            providerBaseUrl: `http://localhost:${process.env.SERVICE_PORT}`,
+            providerVersion: '1.0.0',
+            publishVerificationResult: true,
+        };
+
+        return new Verifier().verifyProvider(opts);
+    });
+
+    afterAll(() => {
+        app.close();
+    })
+})
--- venues-service/__tests__/pact/publish.js
+++ venues-service/__tests__/pact/publish.js
@@ -1,7 +1,7 @@
 const publisher = require('@pact-foundation/pact-node')
 const path = require('path')
 
-let opts = {
+const opts = {
   pactFilesOrDirs: [path.resolve(__dirname, 'contracts')],
   pactBroker: process.env.PACT_BROKER_URL || 'https://limitless-hamlet-75386.herokuapp.com/',
   pactBrokerUsername: process.env.PACT_BROKER_USERNAME || 'admin',
